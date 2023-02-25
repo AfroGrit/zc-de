38eb0f43fca7921a6a5c9ffce5abb5df267429c4
Jean Luc Kitunka <19troubadour@gmail.com>
2023-02-25 18:47:52 +0000

    update model on the staging schema

--- module-4/dbt/models/staging/stg_green.sql
+++ module-4/dbt/models/staging/stg_green.sql
@@ -3,6 +3,13 @@
 -- limit 100
 {{ config(materialized="view") }}
 
+with
+    tripdata as (
+        select *, row_number() over (partition by vendorid, lpep_pickup_datetime) as rn
+        from {{ source("staging", "dbt-green") }}
+        where vendorid is not null
+    )
+
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(["vendorid", "lpep_pickup_datetime"]) }} as tripid,
@@ -31,12 +38,8 @@ select
     {{ get_payment_type_description("payment_type") }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source("staging", "dbt-green") }}
-where vendorid is not null
+from tripdata
+where rn = 1
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
-{% if var('is_test_run', default=true) %}
-
-  limit 100
-
-{% endif %}
+{% if var("is_test_run", default=true) %} limit 100 {% endif %}
--- module-4/dbt/models/staging/stg_yellow.sql
+++ module-4/dbt/models/staging/stg_yellow.sql
@@ -1,4 +1,15 @@
+-- {{ config(materialized="view") }}
+-- select * from {{ source('staging', 'dbt-green') }}
+-- limit 100
 {{ config(materialized="view") }}
+
+with
+    tripdata as (
+        select *, row_number() over (partition by vendorid, lpep_pickup_datetime) as rn
+        from {{ source("staging", "dbt-yellow") }}
+        where vendorid is not null
+    )
+
 
 select
     -- identifiers
@@ -32,8 +43,8 @@ select
     {{ get_payment_type_description("payment_type") }} as payment_type_description,
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
-from {{ source("staging", "dbt-yellow") }}
-where vendorid is not null
+from tripdata
+where rn = 1
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var("is_test_run", default=true) %} limit 100 {% endif %}
