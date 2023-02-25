36d50390b94d085f2f04d347714523622175ba5d
Jean Luc Kitunka <19troubadour@gmail.com>
2023-02-25 19:29:55 +0000

    feat: deploying of dbt models

--- module-4/dbt/models/staging/stg_yellow.sql
+++ module-4/dbt/models/staging/stg_yellow.sql
@@ -5,7 +5,7 @@
 
 with
     tripdata as (
-        select *, row_number() over (partition by vendorid, lpep_pickup_datetime) as rn
+        select *, row_number() over (partition by vendorid, tpep_pickup_datetime) as rn
         from {{ source("staging", "dbt-yellow") }}
         where vendorid is not null
     )
@@ -48,3 +48,4 @@ where rn = 1
 
 -- dbt build --m <model.sql> --var 'is_test_run: false'
 {% if var("is_test_run", default=true) %} limit 100 {% endif %}
+
