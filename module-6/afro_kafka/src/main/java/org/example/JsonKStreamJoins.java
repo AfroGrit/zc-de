0b83a41a7571a71b69b1c3ee724478a5fc900bee
AfroGrit <jlkitunka@gmail.com>
2023-03-14 10:00:10 +0000

    feat stream joins complete, had to add topics in confluent

--- module-6/afro_kafka/src/main/java/org/example/JsonKStreamJoins.java
+++ module-6/afro_kafka/src/main/java/org/example/JsonKStreamJoins.java
@@ -26,6 +26,7 @@ public class JsonKStreamJoins {
         props.put("sasl.mechanism", "PLAIN");
         props.put("client.dns.lookup", "use_all_dns_ips");
         props.put("session.timeout.ms", "45000");
+        // props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka_tutorial.kstream.joined.rides.pickuplocation.v1");
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka_tutorial.kstream.joined.rides.pickuplocation.v1");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
@@ -51,9 +52,26 @@ public class JsonKStreamJoins {
         return streamsBuilder.build();
     }
 
+    public void joinRidesPickupLocation() throws InterruptedException {
+        var topology = createTopology();
+        var kStreams = new KafkaStreams(topology, props);
 
+        kStreams.setUncaughtExceptionHandler(exception -> {
+            System.out.println(exception.getMessage());
+            return StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.SHUTDOWN_APPLICATION;
+        });
+        kStreams.start();
+        while (kStreams.state() != KafkaStreams.State.RUNNING) {
+            System.out.println(kStreams.state());
+            Thread.sleep(1000);
+        }
+        System.out.println(kStreams.state());
+        Runtime.getRuntime().addShutdownHook(new Thread(kStreams::close));
+
+    }
 
     public static void main(String[] args) throws InterruptedException {
-
+        var object = new JsonKStreamJoins();
+        object.joinRidesPickupLocation();
     }
 }
